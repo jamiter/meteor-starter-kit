baaae434da957e5953e78941a177332bf01591ef
Michiel ter Reehorst <michiel@swydo.com>
2017-11-15 21:38:15 +0100

    Subscribe to updates on the client
    Somebody help me please to put this in a better place ;)

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import React from 'react';
+import gql from 'graphql-tag';
 
 import ApolloClient from 'apollo-client';
 import { DDPLink } from 'meteor/swydo:ddp-apollo';
@@ -21,4 +22,28 @@ Meteor.startup(() => {
     </ApolloProvider>,
     document.getElementById('app')
   );
+});
+
+/*
+ * We might be interested in updates on the user too, so let's subscribe to it
+ * I'm not sure what the best place for this would be, so I'm dumping it here for now
+ * A more advanced React developer might be able to help put this where it should
+ */
+const SUBSCRIBE_USER_RANDOM_CHANGES = gql`
+  subscription userChange {
+    userChange {
+      _id
+      randomString
+    }
+  }
+`;
+
+const observer = client.subscribe({
+  query: SUBSCRIBE_USER_RANDOM_CHANGES,
+});
+
+observer.subscribe({
+  next() {
+    // We _could_ do stuff with the new user here, but the UI will update automatically anyway!
+  },
 });
